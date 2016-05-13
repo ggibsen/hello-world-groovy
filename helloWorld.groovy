448cedf20431196786cb7f7b7f821db407c805a4
ggibsen <ggibsen@users.noreply.github.com>
2016-05-12 22:10:33 -0700

    hello world project for Groovy learning

---
+++ helloWorld.groovy
@@ -0,0 +1,30 @@
+/*
+public class HelloWorld {
+	private String name;
+	public void setName(String name) {
+		this.name = name;
+	}    
+	public String getName() {
+		return name;
+	}    
+	public String greet() {
+		return "Hello " + name;
+	}        
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();        
+		helloWorld.setName("Groovy");        
+		System.out.println( helloWorld.greet() );
+	}
+}
+*/
+
+ class HelloWorld {
+	 def name    
+	 def greet() {
+		 "Hello ${name}"
+	 }
+ }
+ def helloWorld = new HelloWorld()
+ helloWorld.name = "Groovy"
+ println helloWorld.greet()
+ 
